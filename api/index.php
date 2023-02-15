d950ded9f144f4240216444e6ffff3c32b73d92b
AbdUlRahman Alkhamees <aaalkhamees@outlook.com>
2023-02-15 21:11:26 +0300

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,8 @@
+<?php
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://github.com/Abd-ulRahman';
+	} else {
+		$uri = 'http://github.com/Abd-ulRahman';
+	}
+?>
+Something is wrong with the XAMPP installation :-(
