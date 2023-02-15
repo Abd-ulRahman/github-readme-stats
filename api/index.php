869f8c36e1082a9ced0421de06781a2dd8059c4f
AbdUlRahman Alkhamees <aaalkhamees@outlook.com>
2023-02-15 21:31:07 +0300

    Delete index.php

--- api/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://github.com/Abd-ulRahman';
-	} else {
-		$uri = 'http://github.com/Abd-ulRahman';
-	}
-?>
-Something is wrong with the XAMPP installation :-(
